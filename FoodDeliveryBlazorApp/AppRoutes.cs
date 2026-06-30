a07eae4623dd69db76f70c3c4836544bff4a9aab
Dmytro Zhyltsov <dmitry.zh100@gmail.com>
2026-06-30 05:49:10 +0300

    Feature/fe 12 ci cd pipelines (#11)
    * ci: deploy Blazor app to GitHub Pages
    
    * fix: base-relative routes for GitHub Pages
    
    * ci: remove comments from deploy workflow
    
    ---------
    
    Co-authored-by: Dmytro Zhyltsov <tomatguitar@gmail.com>

--- FoodDeliveryBlazorApp/AppRoutes.cs
+++ FoodDeliveryBlazorApp/AppRoutes.cs
@@ -2,10 +2,10 @@ namespace FoodDeliveryBlazorApp;
 
 public static class AppRoutes
 {
-    public const string Welcome = "/";
-    public const string Onboarding = "/onboarding";
-    public const string Login = "/login";
-    public const string SignUp = "/signup";
-    public const string Home = "/home";
-    public const string Profile = "/profile";
+    public const string Welcome = "";
+    public const string Onboarding = "onboarding";
+    public const string Login = "login";
+    public const string SignUp = "signup";
+    public const string Home = "home";
+    public const string Profile = "profile";
 }
